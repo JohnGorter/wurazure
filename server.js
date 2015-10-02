f43a0c5bb0279ebd6d889439544eae49a9d1506d
John Gorter <John.Gorter@gmail.com>
2015-10-02 14:06:17 +0200

    Init

---
+++ server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.use(express.static('app'));
+
+app.get("/students", function(req, res){
+   var students = [{ id:1, firstname:"john", lastname:"Gorter"}, { id:2, firstname:"john", lastname:"Gorter"}]
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(students));
+  
+}); 
+app.get('/john', function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+
+app.listen(1337);
+
+console.log('Server running at http://127.0.0.1:1337/');
